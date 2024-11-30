2446572cc8f93a6d9dc6ad495523787eb05d4db3
Abdullah Mohamed <128763899+Sicariusa@users.noreply.github.com>
2024-12-01 00:55:18 +0200

    added rabbit module

--- backend/src/transactions/module/transactions.module.ts
+++ backend/src/transactions/module/transactions.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionsController } from '../controller/transactions.controller';
-import { TransactionsService } from '../services/transactions.service';
-import { AuthService } from '../services/auth.service';
 import { Transactions } from '../schema/TransactionsSchema';
+import { TransactionsService } from '../services/transactions.service';
+import { RabbitMQModule } from './rabbitmq.module';
+// Import RabbitMQModule
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transactions])],
+  imports: [
+    TypeOrmModule.forFeature([Transactions]),
+    RabbitMQModule, // Import RabbitMQModule here
+  ],
   controllers: [TransactionsController],
-  providers: [TransactionsService, AuthService],
+  providers: [TransactionsService],
 })
 export class TransactionsModule {}
