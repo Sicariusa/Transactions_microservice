b59ff80f504e49be518203735c6058075c7784ec
AHegab <amhegab305@gmail.com>
2024-12-26 02:19:22 +0200

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
 
     app.enableCors({
-        origin: 'http://localhost:3000', // Allow only the frontend origin
+        origin: 'https://personalfinancetracker-production-f962.up.railway.app', // Allow only the frontend origin
         credentials: true, // Allow credentials (cookies, headers, etc.)
       });
     app.useGlobalPipes(new ValidationPipe({ transform: true }));
